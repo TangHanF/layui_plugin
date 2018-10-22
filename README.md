9ac8d57621dab7415ebabc1f1ffdc604f6d9602b
TangHanF <guofu_gh@163.com>
2018-10-22 09:27:02 +0800

    tabrightmenu版本更新到V1.0.1

--- README.md
+++ README.md
@@ -1,32 +1,75 @@
 # layui_plugin
 LayUI扩展模块，持续增加中...
 
-# Tab选项卡右键弹出菜单
+-------
 
-> 为Tab选项卡增加右键弹出菜单，例如：关闭当前、关闭其它....
->
-> 默认右键菜单弹出项有:
-> - 关闭当前（closethis）
-> - 关闭所有（closeall）
-> - 关闭其它（closeothers）
-> - 关闭左侧所有（closeleft）
-> - 关闭右侧所有（closeright）
-> - 刷新当前页（refresh）
->
-> 当然，可以根据实际情况自动选择部分内容进行渲染（调用方式下文有描述）
+[TOC]
 
-## 名词定义
+-----
 
-> - **自定义弹出项**:除组件默认提供的6个右键菜单弹出项外其它所有新增加的菜单项都为`自定义弹出项`
+# 一、Tab选项卡右键弹出菜单
+**组件名称：** `tabrightmenu`
 
-## 功能支持
+**实例方法：**`render`
 
-- 支持页面多个Tab选项卡弹出菜单单独定义
-- 支持自定义弹出项和弹出事件（使用`registMethod`属性进行注册）
+> tabrightmenu.render(options)
 
-- 支持自定义图标（目前仅支持LayUI默认图标）
+## 1.1 options说明
 
-### 效果图
+|   属性名称   | 属性说明               | 备注                                                         | 类型                    |
+| :----------: | :--------------------- | :----------------------------------------------------------- | ----------------------- |
+|  container   | 选项卡外部容器ID       | 例如：`container: '#nav1'`                                   | String,''               |
+|    filter    | 过滤值                 | div的”lay-filter“属性。<br />注意：ul的id值要和lay-filter属性一致，参考下面实例即可 | String,''               |
+| pinTabTitles | 固定标签的标题集合     | 通过指定的标签标题集合告诉组件哪些标签是固定的<br />从而在一些操作事件中进行相应处理，<br />例如”关闭其它但固定“等操作 | Array,[]                |
+|  pintabIDs   | 固定标签的ID集合       | 与`pinTabTitles`属性类似，只不过变为指定ID集合。<br />二者之中可以二选一（建议）也可以都保留，<br />只是便于开发使用考虑加入标题和ID形式进行两种方式的过滤 | Array,[]                |
+|    width     | 弹出菜单的宽度         | 一般情况下100~110即可                                        | int                     |
+|    navArr    | 导航标签页配置对象集合 | 对象配置分两大类：<br />1、{eventName: 'xxx', title: 'xxx', icon: 'xxx'}或者{eventName: 'xxx', title: 'xxx'},即`eventName`和`title`是必选项<br />2、{eventName: 'xxx'},只有`eventName`一项，目前取值仅为`line`表示分割线<br />**eventName默认取值：**<br />1) closethis:关闭当前<br />2) closeallbutpin:关闭所有但固定<br />3) closeothersbutpin:关闭非当前但固定<br />4) closeleftbutpin:关闭左侧全部但固定<br />5) closerightbutpin:关闭右侧全部但固定<br />6) closeall:关闭所有<br />7) closeothers:关闭非当前<br />8) closeleft:关闭左侧全部<br />9) closeright:关闭右侧全部<br /> | Array，[{'xx':'xx1'}]   |
+| registMethod | 自定义事件注册         | 在`navArr`配置对象的`eventName`非默认取值时需要使用``registMethod`进行注册，以便组件能够顺利回调方法 | Object,[{{'xx':'xx1'}}] |
+
+**完整示例：**
+
+```javascript
+rightmenu_.render({
+    container: '#nav2',
+    filter: 'main_tab2',
+    // navArr：对象数组，每个对象包含dataType、title、method属性
+    navArr: [
+        {eventName: 'closethis', title: '关闭当前', icon: 'layui-icon-close'},
+        {eventName: 'closeallbutpin', title: '关闭所有但固定'},
+        {eventName: 'closeothersbutpin', title: '关闭其它但固定'},
+        {eventName: 'line'},// 创建分割线
+
+        {eventName: 'closeall', title: '关闭所有'},
+        {eventName: 'closeothers', title: '关闭其它'},
+        {eventName: 'closeleftbutpin', title: '关闭左侧'},
+        {eventName: 'closerightbutpin', title: '关闭右侧'},
+        {eventName: 'line'},// 创建分割线
+
+        //自定义菜单项，此时dataType属性表示自定义事件
+        {title: "测试", eventName: 'test'},
+        {title: "测试2", eventName: 'test2'},
+    ],
+    // 注册自定义事件
+    registMethod: {'test': testMethod, 'test2': test2Method}
+})
+
+function testMethod(id, title, currentNavGroup, dom) {
+    changeCurrentTabContent(currentNavGroup, "[测试]触发,触发的ID：" + id + "，标题：" + title);
+}
+
+function test2Method(id, title, currentNavGroup, dom) {
+    changeCurrentTabContent(currentNavGroup, "[测试2]触发,触发的ID：" + id + "，标题：" + title);
+}
+
+function changeCurrentTabContent(currentNavGroup, content) {
+    $(currentNavGroup + " div[class='layui-tab-item layui-show']").html(content);
+
+}
+```
+
+> 以上基本就是一个完整的组件内部属性使用示例，可以适当拆分组合属性已达到预期效果。
+
+## 1.2 效果图
 
 - 一个页面一个Tab选项卡情况
 
@@ -38,13 +81,63 @@ LayUI扩展模块，持续增加中...
 
 ![](https://ws1.sinaimg.cn/large/006tNbRwly1fw568pduerj30zo0mgwgf.jpg)
 
-### 示例地址：[点此进入](https://tanghanf.github.io/layui_plugin/rightmenu_demo.html)
+## 1.3 示例地址
 
-### 调用说明
+[点此进入](https://tanghanf.github.io/layui_plugin/rightmenu_demo.html)
 
-> #### 一个页面一个Tab选项卡情况
+## 1.4 更新历史
 
-##### html部分
+- **V1.0.0** 2018年10月12日
+
+    初始版本
+
+- **V1.0.1** 2018年10月19日
+
+    > 感谢LayUI社区：[未闻花名0821](https://fly.layui.com/u/19689936/)提供建议以及代码，在其基础之后又做了调整，可参考我原帖地址以及采纳的答案，[点此直达](https://fly.layui.com/jie/24715/)
+
+    1.增加对固定标签页的处理，关闭时默认不关闭已固定的标签页（也提供eventName类型提供关闭固定标签页）。
+
+    2.把let和const改为var，兼容低版本IE浏览器（唉😌，无奈.....）
+
+    3.弹出项支持配置分割线（navArr集合对象修改为`eventName:'line'`即可）
+
+-----
+
+> 为Tab选项卡增加右键弹出菜单，例如：关闭当前、关闭其它....
+>
+> 默认右键菜单弹出项有:
+> - 关闭当前（closethis）
+> - 关闭所有但固定（closeallbutpin）
+> - 关闭其它但固定（closeothersbutpin）
+> - 关闭左侧所有但固定（closeleftbutpin）
+> - 关闭右侧所有但固定（closerightbutpin）
+> - 刷新当前页（refresh）
+>
+> 额外的默认事件（非默认弹出，需要自己根据实际情况制定）：
+>
+> - 关闭所有（closeall）
+> - 关闭其它（closeother）
+> - 关闭左侧所有（closeleft）
+> - 关闭右侧所有（closeright）
+>
+> 当然，可以根据实际情况自动选择部分内容进行渲染（调用方式下文有描述）
+
+## 1.5 名词定义
+
+> - **自定义弹出项**:除组件默认提供的6个右键菜单弹出项外其它所有新增加的菜单项都为`自定义弹出项`
+
+## 1.6 功能支持
+
+- 支持页面多个Tab选项卡弹出菜单单独定义
+- 支持自定义弹出项和弹出事件（使用`registMethod`属性进行注册）
+
+- 支持自定义图标（目前仅支持LayUI默认图标）
+
+## 1.7 调用说明
+
+### 1.7.1 一个页面一个Tab选项卡情况
+
+**html部分**
 
 Tab选项卡使用官方提供模板： https://www.layui.com/doc/element/tab.html 
 然后简单修改如下：
@@ -79,9 +172,9 @@ Tab选项卡使用官方提供模板： https://www.layui.com/doc/element/tab.ht
 >
 > 其它内容自行处理即可！
 
-##### JavaScript部分
+**JavaScript部分**
 
-> 默认渲染
+> ##### 默认渲染
 
 ```javascript
 layui.config({
@@ -90,7 +183,7 @@ layui.config({
     let element = layui.element;
     let rightmenu_ = layui.tabrightmenu;
 
-    // 默认方式渲染全部：关闭当前（closethis）、关闭所有（closeall）、关闭其它（closeothers）、关闭左侧所有（closeleft）、关闭右侧所有（closeright）、刷新当前页（refresh）
+    // 默认方式渲染全部：关闭当前（closethis）、关闭所有但固定（closeallbutpin）、关闭其它但固定（closeothersbutpin）、关闭左侧所有但固定（closeleftbutpin）、关闭右侧所有但固定（closerightbutpin）、刷新当前页（refresh）
     rightmenu_.render({
         container: '#nav1',
         filter: 'main_tab1',
@@ -107,14 +200,14 @@ layui.config({
     let element = layui.element;
     let rightmenu_ = layui.tabrightmenu;
 
-    // 默认方式渲染全部：关闭当前（closethis）、关闭所有（closeall）、关闭其它（closeothers）、关闭左侧所有（closeleft）、关闭右侧所有（closeright）、刷新当前页（refresh）
+    // 默认方式渲染全部：关闭当前（closethis）、关闭所有但固定（closeallbutpin）、关闭其它但固定（closeothersbutpin）、关闭左侧所有但固定（closeleftbutpin）、关闭右侧所有但固定（closerightbutpin）、刷新当前页（refresh）
     rightmenu_.render({
         container: '#nav1',
         filter: 'main_tab1',
         navArr: [
             {eventName: 'closethis', title: '关闭当前', icon: 'layui-icon-close'},
-            {eventName: 'closeall', title: '关闭所有'},
-            {eventName: 'closeothers', title: '关闭其它'},
+            {eventName: 'closeallbutpin', title: '关闭所有'},
+            {eventName: 'closeothersbutpin', title: '关闭其它'},
             //自定义菜单项，此时dataType属性表示自定义事件
             {title: "测试", eventName: 'test'},
             {title: "测试2", eventName: 'test2'},
@@ -143,7 +236,7 @@ layui.config({
 > - 使用`自定义弹出项`最重要的就是`navArr`属性。该属性是一个对象集合，每一个对象描述了菜单项的一些基本信息（eventName：事件类型、title：菜单项标题、icon：图标class类名）
 > - 使用`自定义弹出项`需要定义`registMethod`属性，以便对`eventName`属性指定的事件进行注册
 
-> #### 一个页面一个Tab选项卡情况
+### 1.7.2 一个页面多个Tab选项卡情况
 
 该情景与上述定义、调用差不多，唯一需要注意的就是HTML部分。
 
@@ -202,7 +295,7 @@ layui.config({
     let element = layui.element;
     let rightmenu_ = layui.tabrightmenu;
 
-    // 默认方式渲染全部：关闭当前（closethis）、关闭所有（closeall）、关闭其它（closeothers）、关闭左侧所有（closeleft）、关闭右侧所有（closeright）、刷新当前页（refresh）
+    // 默认方式渲染全部：关闭当前（closethis）、关闭所有但固定（closeallbutpin）、关闭其它但固定（closeothersbutpin）、关闭左侧所有但固定（closeleftbutpin）、关闭右侧所有但固定（closerightbutpin）、刷新当前页（refresh）
     // 渲染nav1
     rightmenu_.render({
         container: '#nav1',
@@ -216,8 +309,8 @@ layui.config({
         // navArr：对象数组，每个对象包含dataType、title、method属性
         navArr: [
             {eventName: 'closethis', title: '关闭当前', icon: 'layui-icon-close'},
-            {eventName: 'closeall', title: '关闭所有'},
-            {eventName: 'closeothers', title: '关闭其它'},
+            {eventName: 'closeallbutpin', title: '关闭所有'},
+            {eventName: 'closeothersbutpin', title: '关闭其它'},
             //自定义菜单项，此时dataType属性表示自定义事件
             {title: "测试", eventName: 'test'},
             {title: "测试2", eventName: 'test2'},
@@ -242,3 +335,60 @@ layui.config({
 ```
 
 以上就可以实现一个页面多个选项卡进行不同处理，实用性更强。
+
+### 1.7.3 混合示例
+
+#### 1.7.3.1 指定固定标签页
+
+```javascript
+rightmenu_.render({
+    container: '#nav1',
+    filter: 'main_tab1',
+
+    // pinTabTitles和pintabIDs作用一样，可以二选一（建议）也可以都保留，只是便于开发使用考虑加入标题和ID形式进行两种方式的过滤
+    pinTabTitles:['网站设置'],//表示标题为“网站设置”的标签为固定标签
+    pintabIDs:['0','1'],//表示id为“0”、"1"的标签为固定标签
+});
+```
+
+#### 1.7.3.2 设置弹出菜单宽度
+
+```javascript
+rightmenu_.render({
+    container: '#nav1',
+    filter: 'main_tab1',
+
+    width:160,// 右键弹出框的宽度，一般100~110即可
+});
+```
+
+#### 1.7.3.3 设置分割线
+
+```javascript
+rightmenu_.render({
+    container: '#nav2',
+    filter: 'main_tab2',
+    // navArr：对象数组，每个对象包含dataType、title、method属性
+    navArr: [
+        {eventName: 'closethis', title: '关闭当前', icon: 'layui-icon-close'},
+        {eventName: 'closeallbutpin', title: '关闭所有但固定'},
+        {eventName: 'closeothersbutpin', title: '关闭其它但固定'},
+        
+        {eventName:'line'},// 创建分割线
+
+        {eventName: 'closeall', title: '关闭所有'},
+        {eventName: 'closeothers', title: '关闭其它'},
+        {eventName: 'closeleftbutpin', title: '关闭左侧'},
+        {eventName: 'closerightbutpin', title: '关闭右侧'},
+        
+        {eventName:'line'},// 创建分割线
+
+        //自定义菜单项，此时dataType属性表示自定义事件
+        {title: "测试", eventName: 'test'},
+        {title: "测试2", eventName: 'test2'},
+    ],
+    // 注册自定义事件
+    registMethod: {'test': testMethod, 'test2': test2Method}
+})
+```
+
--- module/tabrightmenu.js
+++ module/tabrightmenu.js
@@ -1,10 +1,11 @@
 layui.define(['element'], function (exports) {
-    let element = layui.element;
-    const $ = layui.jquery;
-
-    let MOD_NAME = 'tabrightmenu';
-    let RIGHTMENUMOD = function () {
-        this.v = '1.0.0';
+    var element = layui.element;
+    var $ = layui.jquery;
+    var currentActiveTabID = undefined;
+    var currentActiveTabTitle = undefined;
+    var MOD_NAME = 'tabrightmenu';
+    var RIGHTMENUMOD = function () {
+        this.v = '1.0.1';
         this.author = 'TangHanF';
         this.email = 'guofu_gh@163.com';
         this.filter = '';//Tab选项卡的事件过滤
@@ -12,8 +13,8 @@ layui.define(['element'], function (exports) {
     };
     String.prototype.format = function () {
         if (arguments.length == 0) return this;
-        let param = arguments[0];
-        let s = this;
+        var param = arguments[0];
+        var s = this;
         if (typeof(param) == 'object') {
             for (var key in param) s = s.replace(new RegExp("\\{" + key + "\\}", "g"), param[key]);
             return s;
@@ -23,9 +24,9 @@ layui.define(['element'], function (exports) {
         }
     }
 
-    function createStyle(ulClassName) {
-        let style = '.{name} {position: absolute;width: 110px;z-index: 9999;display: none;background-color: #fff;padding: 2px;color: #333;border: 1px solid #eee;border-radius: 2px;cursor: pointer;}.{name} li {text-align: center;display: block;height: 30px;line-height: 32px;}.{name} li:hover {background-color: #666;color: #fff;}'
-            .format({name: ulClassName});
+    function createStyle(ulClassName, width) {
+        var style = '.{name} {position: absolute;width: {width_}px;z-index: 9999;display: none;background-color: #fff;padding: 2px;color: #333;border: 1px solid #eee;border-radius: 2px;cursor: pointer;}.{name} li {text-align: center;display: block;height: 30px;line-height: 32px;}.{name} li:hover {background-color: #666;color: #fff;}'
+            .format({name: ulClassName, width_: width});
         return style;
     }
 
@@ -33,8 +34,6 @@ layui.define(['element'], function (exports) {
      * 初始化
      */
     RIGHTMENUMOD.prototype.render = function (opt) {
-        createStyle();
-
         if (!opt.container) {
             console.error("[ERROR]使用rightmenu组件需要制定'container'属性！");
             return;
@@ -44,14 +43,19 @@ layui.define(['element'], function (exports) {
             return;
         }
         this.filter = opt.filter;
+        this.width = opt.width ? opt.width : 110;// 右键弹出框的宽度，一般100~110即可
 
-        let defaultNavArr = [
-            {eventName: 'closethis', title: '关闭当前'},
-            {eventName: 'closeall', title: '关闭所有'},
-            {eventName: 'closeothers', title: '关闭其它'},
-            {eventName: 'closeleft', title: '关闭左侧所有'},
-            {eventName: 'closeright', title: '关闭右侧所有'},
+        // pinTabTitles和pintabIDs作用一样，只是便于开发使用考虑加入标题和ID形式进行两种方式的过滤
+        this.pinTabTitles = opt.pinTabTitles;//固定标签的标题集合
+        this.pintabIDs = opt.pintabIDs;//固定标签的ID集合
+
+        var defaultNavArr = [
             {eventName: 'refresh', title: '刷新当前页'},
+            {eventName: 'closeallbutpin', title: '关闭所有但固定'},
+            {eventName: 'closethis', title: '关闭标签'},
+            {eventName: 'closeothersbutpin', title: '关闭其它但固定'},
+            {eventName: 'closeleftbutpin', title: '关闭左侧标签但固定'},
+            {eventName: 'closerightbutpin', title: '关闭右侧标签但固定'}
         ];
         opt = opt || {};
         opt.navArr = opt.navArr || defaultNavArr;
@@ -66,14 +70,16 @@ layui.define(['element'], function (exports) {
      */
     function CreateRightMenu(rightMenuConfig) {
         // 使用"filter"属性作为css样式名称
-        $("<style></style>").text(createStyle(rightMenuConfig.filter)).appendTo($("head"));
-        let li = '';
+        $("<style></style>").text(createStyle(rightMenuConfig.filter, rightMenuConfig.width)).appendTo($("head"));
+        var li = '';
         $.each(rightMenuConfig.navArr, function (index, conf) {
-            // li += '<li data-type="' + conf.eventName + '"><i class="layui-icon layui-icon-face-smile"></i> ' + conf.title + '</li>';
-            li += '<li data-type="{eventName}"><i class="layui-icon {icon}"></i>{title}</li>'
-                .format({eventName:conf.eventName,icon:conf.icon?conf.icon:"",title:conf.title});
+            if (conf.eventName === 'line')
+                li += '<hr/>';
+            else
+                li += '<li data-type="{eventName}"><i class="layui-icon {icon}"></i>{title}</li>'
+                    .format({eventName: conf.eventName, icon: conf.icon ? conf.icon : "", title: conf.title});
         })
-        let tmpHtml = '<ul class="{className}">{liStr} </ul>'.format({liStr: li, className: rightMenuConfig.filter})
+        var tmpHtml = '<ul class="{className}">{liStr} </ul>'.format({liStr: li, className: rightMenuConfig.filter})
         $(rightMenuConfig.container).after(tmpHtml);
         _CustomRightClick(rightMenuConfig);
     }
@@ -84,39 +90,61 @@ layui.define(['element'], function (exports) {
      * @constructor
      */
     function _CustomRightClick(rightMenuConfig) {
-
         //屏蔽Tab右键
-        $("#" + rightmenuObj.filter + ' li').on('contextmenu', function () {
-            return false;
-        })
+        //$("#" + rightmenuObj.filter + ' li').on('contextmenu', function (e) {
+        //    return false;
+        //})
         $('#' + rightmenuObj.filter + ',' + rightmenuObj.filter + ' li').click(function () {
             $('.' + rightMenuConfig.filter).hide();
         });
-        console.log("[事件绑定，元素]" + rightmenuObj.filter + ' li')
-        $("#" + rightmenuObj.filter + ' li').on('contextmenu', function (e) {
-            let popupmenu = $("." + rightMenuConfig.filter);
-            let leftValue = ($(document).width() - e.clientX) < popupmenu.width() ? (e.clientX - popupmenu.width()) : e.clientX;
-            let topValue = ($(document).height() - e.clientY) < popupmenu.height() ? (e.clientY - popupmenu.height()) : e.clientY;
+        //事件委托，处理动态添加的li
+        $("#" + rightmenuObj.filter).on('contextmenu', 'li', function (e) {
+            currentActiveTabID = $(e.target).attr('lay-id');// 获取当前激活的选项卡ID,当前ID改为右键菜单弹出时就获取
+            currentActiveTabTitle = $.trim($(e.target).text());//获取当前激活选项卡的标题
+
+            if (rightMenuConfig.pinTabTitles && rightMenuConfig.pinTabTitles.indexOf(currentActiveTabTitle) != -1 || currentActiveTabTitle == undefined) {   //特殊ID，弹出默认的右键菜单
+                return true;
+            }
+            if (rightMenuConfig.pintabIDs && rightMenuConfig.pintabIDs.indexOf(currentActiveTabID) != -1 || currentActiveTabID == undefined) {   //特殊ID，弹出默认的右键菜单
+                return true;
+            }
+            var popupmenu = $("." + rightMenuConfig.filter);
+            var leftValue = ($(document).width() - e.clientX) < popupmenu.width() ? (e.clientX - popupmenu.width()) : e.clientX;
+            var topValue = ($(document).height() - e.clientY) < popupmenu.height() ? (e.clientY - popupmenu.height()) : e.clientY;
             popupmenu.css({left: leftValue, top: topValue}).show();
             return false;
         });
+
         // 点击空白处隐藏弹出菜单
         $(document).click(function (event) {
             event.stopPropagation();
             $("." + rightMenuConfig.filter).hide();
         });
 
-
         /**
          * 注册tab右键菜单点击事件
          */
-        console.log("[注册tab右键菜单点击事件]" + '.' + rightMenuConfig.filter + ' li');
-        $('.' + rightMenuConfig.filter + ' li').click(function () {
-            let currentActiveTabID = $("#" + rightMenuConfig.filter + ">li[class='layui-this']").attr('lay-id');// 获取当前激活的选项卡ID
-            let tabtitle = $("#" + rightMenuConfig.filter + " li");
-            let allTabIDArr = [];
-            $.each(tabtitle, function (i) {
-                allTabIDArr[i] = $(this).attr("lay-id");
+        $('.' + rightMenuConfig.filter + ' li').click(function (e) {
+            var tabs = $("#" + rightMenuConfig.filter + " li");
+            var allTabIDArrButPin = [];//所有非固定选项卡集合
+            var allTabIDArr = [];// 所有选项卡集合
+            var idIndexButPin = 0;
+            var idIndex = 0;
+            $.each(tabs, function (i) {
+                var id = $(this).attr("lay-id");
+                var title = $(this).text();
+                if (rightMenuConfig.pinTabTitles == undefined || rightMenuConfig.pintabIDs == undefined) {
+                    allTabIDArrButPin[idIndexButPin] = id;
+                    idIndexButPin++;
+                }
+                else if ((rightMenuConfig.pinTabTitles && rightMenuConfig.pinTabTitles.indexOf(title) == -1) &&
+                    ((rightMenuConfig.pintabIDs && rightMenuConfig.pintabIDs.indexOf(id) == -1) || id == undefined))  //去除特殊ID
+                {
+                    allTabIDArrButPin[idIndexButPin] = id;
+                    idIndexButPin++;
+                }
+                allTabIDArr[idIndex] = id;
+                idIndex++;
             })
 
             // 事件处理
@@ -124,43 +152,59 @@ layui.define(['element'], function (exports) {
                 case "closethis"://关闭当前，如果开始了tab可关闭，实际意义不大
                     tabDelete(currentActiveTabID, rightMenuConfig.filter);
                     break;
+                case "closeallbutpin"://关闭所有但固定
+                    tabDeleteAll(allTabIDArrButPin, rightMenuConfig.filter);
+                    break;
+                case "closeothersbutpin"://关闭非当前但固定
+                    $.each(allTabIDArrButPin, function (i) {
+                        var tmpTabID = allTabIDArrButPin[i];
+                        if (currentActiveTabID != tmpTabID)
+                            tabDelete(tmpTabID, rightMenuConfig.filter);
+                    })
+                    break;
+                case "closeleftbutpin"://关闭左侧全部但固定
+                    var indexCloseleft = allTabIDArrButPin.indexOf(currentActiveTabID);
+                    console.log(allTabIDArrButPin.slice(0, indexCloseleft));
+                    tabDeleteAll(allTabIDArrButPin.slice(0, indexCloseleft), rightMenuConfig.filter);
+                    break;
+                case "closerightbutpin"://关闭右侧全部但固定
+                    var indexCloseright = allTabIDArrButPin.indexOf(currentActiveTabID);
+                    tabDeleteAll(allTabIDArrButPin.slice(indexCloseright + 1), rightMenuConfig.filter);
+                    break;
+
                 case "closeall"://关闭所有
                     tabDeleteAll(allTabIDArr, rightMenuConfig.filter);
                     break;
                 case "closeothers"://关闭非当前
                     $.each(allTabIDArr, function (i) {
-                        let tmpTabID = allTabIDArr[i];
+                        var tmpTabID = allTabIDArr[i];
                         if (currentActiveTabID != tmpTabID)
                             tabDelete(tmpTabID, rightMenuConfig.filter);
                     })
                     break;
                 case "closeleft"://关闭左侧全部
-                    let indexCloseleft = allTabIDArr.indexOf(currentActiveTabID);
-                    tabDeleteAll(allTabIDArr.slice(0, indexCloseleft));
+                    var indexCloseleft = allTabIDArr.indexOf(currentActiveTabID);
+                    tabDeleteAll(allTabIDArr.slice(0, indexCloseleft), rightMenuConfig.filter);
                     break;
                 case "closeright"://关闭右侧全部
-                    let indexCloseright = allTabIDArr.indexOf(currentActiveTabID);
-                    tabDeleteAll(allTabIDArr.slice(indexCloseright + 1));
+                    var indexCloseright = allTabIDArr.indexOf(currentActiveTabID);
+                    tabDeleteAll(allTabIDArr.slice(indexCloseright + 1), rightMenuConfig.filter);
                     break;
                 case "refresh":
-                    // 重新加载iFrame，实现更新。注意：如果你不是使用的iFrame则无效，具体刷新实现自行处理
-                    // document.getElementById(currentActiveTabID).contentWindow.location.reload(true);
                     refreshiFrame();
                     break;
                 default:
-                    let currentTitle = $("#" + rightMenuConfig.filter + ">li[class='layui-this']").text();
+                    var currentTitle = $("#" + rightMenuConfig.filter + ">li[class='layui-this']").text();
                     rightMenuConfig.registMethod[$(this).attr("data-type")](currentActiveTabID, currentTitle, rightMenuConfig.container, $(this)[0]);
-
             }
             $('.rightmenu').hide();
         })
     }
 
-    let tabDelete = function (id, currentNav) {
-        console.log("删除的TabID：" + id + ",所属组：" + currentNav);
+    var tabDelete = function (id, currentNav) {
         element.tabDelete(currentNav, id);//删除
     }
-    let tabDeleteAll = function (ids, currentNav) {
+    var tabDeleteAll = function (ids, currentNav) {
         $.each(ids, function (i, item) {
             element.tabDelete(currentNav, item);
         })
@@ -169,12 +213,12 @@ layui.define(['element'], function (exports) {
      * 重新加载iFrame，实现更新
      * @returns {boolean}
      */
-    let refreshiFrame = function () {
-        let $curFrame = $('iframe:visible');
+    var refreshiFrame = function () {
+        var $curFrame = $('iframe:visible');
         $curFrame.attr("src", $curFrame.attr("src"));
         return false;
     }
 
-    let rightmenuObj = new RIGHTMENUMOD();
+    var rightmenuObj = new RIGHTMENUMOD();
     exports(MOD_NAME, rightmenuObj);
 })
